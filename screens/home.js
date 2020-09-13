b83d4eab3da51e303a16ab09f4a572a94ca1cd5f
Snigdha Sanat <snigdhasanat@Snigdhas-Mac.local>
2020-09-13 13:22:58 +0530

    Form validation

--- screens/home.js
+++ screens/home.js
@@ -4,12 +4,15 @@ import {
   Text,
   FlatList,
   TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
   Modal,
   StyleSheet,
 } from "react-native";
 import { globalStyles } from "../styles/global";
 import Card from "../shared/card";
 import { MaterialIcons } from "@expo/vector-icons";
+import ReviewForm from "./reviewForm";
 
 export default function Home({ navigation }) {
   const [modalOpen, setModalOpen] = useState("false");
@@ -33,20 +36,31 @@ export default function Home({ navigation }) {
       key: "3",
     },
   ]);
+
+  const addReview = (review) => {
+    review.key = Math.random().toString();
+    setReviews((currentReviews) => {
+      return [review, ...currentReviews];
+    });
+    setModalOpen(false);
+  };
+
   return (
     <View style={globalStyles.container}>
       <Modal visible={modalOpen} animationType="slide">
-        <View style={styles.modalContent}>
-          <MaterialIcons
-            name="close"
-            size={24}
-            style={{ ...styles.modalToggle, ...styles.modalClose }}
-            onPress={() => {
-              setModalOpen(false);
-            }}
-          />
-          <Text>Hello from the modal :)</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.modalContent}>
+            <MaterialIcons
+              name="close"
+              size={24}
+              style={{ ...styles.modalToggle, ...styles.modalClose }}
+              onPress={() => {
+                setModalOpen(false);
+              }}
+            />
+            <ReviewForm addReview={addReview} />
+          </View>
+        </TouchableWithoutFeedback>
       </Modal>
 
       <MaterialIcons
